872462e10c21ee2624b63c02533a9e44ccb181c2
meetkamal256 <meetkamal256@gmail.com>
2025-09-03 22:52:40 +0100

    feat: add dark/light theme support with CSS variables and globals.css update

---
+++ app/_components/ThemeToggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import styles from "./ThemeToggle.module.css"; 
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
+  if (!mounted) return null;
+  
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={styles.toggleBtn}
+    >
+      {theme === "light" ? <FaMoon /> : <FaSun />}
+    </button>
+  );
+};
+
+export default ThemeToggle;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ThemeToggle from "./_components/ThemeToggle";
 
 export default function Home() {
   return (
     <>
-    <div>Hello world</div>
+      <ThemeToggle />
+
+      <div className={styles.home}>
+        <h1 className={styles.heading}>Next.js Dark mode tutorial</h1>
+        <p className={styles.subText}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, iure.
+          Impedit, explicabo dignissimos necessitatibus tenetur voluptates
+          molestias exercitationem nobis autem officia dolorum fugit porro vitae
+          ducimus beatae laborum aperiam recusandae.
+        </p>
+        <div className={styles.btnContainer}>
+          <button className={styles.btnPrimary}>Button 1</button>
+          <button className={styles.btnSecondary}>Button 2</button>
+        </div>
+      </div>
     </>
   );
 }
