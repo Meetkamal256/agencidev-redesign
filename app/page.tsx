22887ac90ffc4906d3d533860fdfd712b826594d
meetkamal256 <meetkamal256@gmail.com>
2025-09-03 23:05:40 +0100

    feat: add dark/light theme support with CSS variables and globals.css update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import ThemeToggle from "./_components/ThemeToggle";
+import ThemeToggle from "./_components/ThemeToggle/ThemeToggle";
 
 export default function Home() {
   return (
