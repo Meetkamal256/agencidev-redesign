29ae22e443cc9430d3bac86706b779beb15ed308
meetkamal256 <meetkamal256@gmail.com>
2025-09-04 06:09:21 +0100

    fix: fix build remove by removing unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import ThemeToggle from "./_components/ThemeToggle/ThemeToggle";
 import Navbar from "./_components/section/Navbar/Navbar";
 import Hero from "./_components/section/Hero/Hero";
 
