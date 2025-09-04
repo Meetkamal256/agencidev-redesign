17a06ce8ebb5332182909d191bfd1e60ef2f78b9
meetkamal256 <meetkamal256@gmail.com>
2025-09-04 23:13:44 +0100

    feat: build faq section for desktop, tablet and mobile

---
+++ app/data.jsx
@@ -0,0 +1,25 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "What services do we offer",
+    answer: "End-to-end product development, content marketing and design",
+  },
+  {
+    id: 2,
+    question: "How much does product development cost?",
+    answer:
+      "For product development, we keep low startup costs and a 10-40% revenue share in the new product. Our product development is end-to-end, covering everything from design &amp; planning to upgrades &amp; marketing. We treat this as a new business, not just a project for a client. We treat each new business as an independent entity, with its own systems and teams. Clients retain majority ownership while we manage operations, ensuring their involvement is as minimal or extensive as they prefer.",
+  },
+  {
+    id: 3,
+    question: "Why do we serve",
+    answer:
+      "Info-product owners doing a minimum of $4,000 /month in profit for our marketing services. For our design services, we work with agencies of any size. We provide our product development services exclusively to agency &amp; info-product owners doing north of $100,000 annually.",
+  },
+  {
+    id: 4,
+    question: "Why are we different",
+    answer:
+      "Agencidev is not a traditional marketing agency focused solely on acquiring clients. We are not a creative agency either focused on design, branding and websites. Instead, we focus on our client’s business ecosystem growth as a whole. We build great agencies. We want to give every client a luxury experience. We don’t just care about the food on the plate. We care about every small detail—from how the food is presented to the temperature of the restaurant to the type of plate material used to the perfume the waiter wears. We pay attention to the details many don’t even know exist. Because when stacked together, these seemingly invisible small details add up to become excellence. And that is what we aim to embody."
+ },
+];
---
+++ app/faq/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./faq.module.css";
+import { faqs } from "../data";
+import Footer from "../_components/section/Footer/Footer";
+
+type FAQ = {
+  question: string;
+  answer: string;
+};
+
+const Page = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleFAQ = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <div className="container">
+      <div className={styles.faqSection}>
+        <h1 className={styles.sectionHeading}>
+          We believe that digital businesses are the future.
+        </h1>
+
+        <div className={styles.buttonWrapper}>
+          <button className={styles.auditButton}>Free Audit</button>
+        </div>
+        
+        <h2 className={styles.subHeading}>Frequently Answered Questions</h2>
+
+        <div className={styles.faqContainer}>
+          {faqs.map((faq: FAQ, index: number) => (
+            <div key={index} className={styles.faqItem}>
+              <div
+                className={styles.faqQuestion}
+                onClick={() => toggleFAQ(index)}
+              >
+                {faq.question}
+                <span>{activeIndex === index ? "−" : "+"}</span>
+              </div>
+              {activeIndex === index && (
+                <div className={styles.faqAnswer}>{faq.answer}</div>
+              )}
+            </div>
+          ))}
+        </div>
+          </div>
+          <Footer />
+    </div>
+  );
+};
+
+export default Page;
